6e6f2671711cdcddf15cb422935d77fba0c9dc40
Branimal17 <142623712+Branimal17@users.noreply.github.com>
2024-05-06 21:57:45 -0400

    Add Choice Logic
    Added the logic to get the computer players
    choice and the human players choice

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Hello World");
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+getHumanChoice();
+getComputerChoice();
+
+
+function getComputerChoice() {
+    let computerChoice;
+    let num = Math.random();
+
+    if (num <= 0.33) {
+        computerChoice = "Rock";
+    }
+    else if (num <= 0.66) {
+        computerChoice = "Paper";
+    }
+    else if (num <= 1) {
+        computerChoice = "Scissors";
+    }
+    console.log("Computer Chose: " + computerChoice);
+    return computerChoice
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please Choose 'Rock', 'Paper', or 'Scissors';")
+    humanChoice = humanChoice.toLowerCase();
+    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
+        if (humanChoice == null) {
+            break;
+        }
+        humanChoice = prompt("Not a valid choice, Please Choose 'Rock', 'Paper', or 'Scissors'");
+    }
+    console.log("Player Chose: " + humanChoice);
+    return humanChoice
+}
+
+function playRound(humanChoice, computerChoice) {
+
+
+}
+
